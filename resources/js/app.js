b396acc4e0659c13f77c5a92a385435b1b32816c
Nuno Lopes <nuno@redweb.dk>
2019-04-19 16:46:04 +0100

    Improve code

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,9 @@ intlTelInput = require('intl-tel-input');
 document.addEventListener('DOMContentLoaded', function(){
     var input = document.querySelector("#phone");
     intlTelInput(input, {
+        utilsScript: require('intl-tel-input/build/js/utils'),
         initialCountry: "pt",
         separateDialCode: 'true',
+        hiddenInput: input.getAttribute('name'),
     });
 }, false);
--- resources/views/contacts/create.blade.php
+++ resources/views/contacts/create.blade.php
@@ -4,23 +4,24 @@
 
 @section('content')
     <h1>Create New Contact</h1>
-    <form>
+    <form method="POST" action="{{ url('contacts') }}">
+        @csrf
         <div class="form-group">
             <label for="first_name">First Name</label>
-            <input type="text" class="form-control" id="first_name" placeholder="Enter first name">
+            <input type="text" class="form-control" name="first_name" id="first_name" placeholder="Enter first name">
         </div>
         <div class="form-group">
             <label for="last_name">Last Name</label>
-            <input type="text" class="form-control" id="last_name" placeholder="Enter last name">
+            <input type="text" class="form-control" name="last_name" id="last_name" placeholder="Enter last name">
         </div>
         <div class="form-group">
             <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email" pattern="\S+@\S+\.\S+">
+            <input type="email" class="form-control" name="email" id="exampleInputEmail1" placeholder="Enter email" pattern="\S+@\S+\.\S+">
         </div>
         <div class="form-group">
             <label for="phone">Phone Number</label>
             <div class="d-block">
-                <input type="tel" class="form-control" id="phone" placeholder="Enter phone number">
+                <input type="tel" class="form-control intl-tel-input" name="phone_number" id="phone">
             </div>
         </div>
         <button type="submit" class="btn btn-success">Create</button>
