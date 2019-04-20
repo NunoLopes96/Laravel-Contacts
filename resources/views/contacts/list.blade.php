3139c3a83033f1c7e2f6aa8517905c35d4e4b056
Nuno Lopes <nuno@redweb.dk>
2019-04-20 13:54:26 +0100

    Removed hard coded data and list contacts from database

--- resources/views/contacts/list.blade.php
+++ resources/views/contacts/list.blade.php
@@ -4,20 +4,29 @@
 
 @section('content')
     <h1>Contacts</h1>
-    <div class="list-group">
-        <div>
-            <a href="{{ url('/contacts/create') }}" class="btn btn-lg btn-outline-success float-right my-2">
-                <i class="fa fa-plus"></i> Create
-            </a>
-        </div>
-        <a href="#" class="list-group-item list-group-item-action">
-            <b>Nuno Lopes</b> <small>(965643427)</small>
-            <span class="btn btn-outline-danger float-right mx-2">
-                    <i class="fa fa-trash"></i>
-            </span>
-            <span class="btn btn-outline-warning float-right mx-2">
-                    <i class="fa fa-edit"></i>
-            </span>
+    <div class="clearfix">
+        <a href="{{ url('/contacts/create') }}" class="btn btn-lg btn-outline-success float-right my-2">
+            <i class="fa fa-plus"></i> Create
         </a>
+    </div>
+    <div>
+        <ul class="list-group">
+        @foreach($contacts as $contact)
+            <li class="list-group-item">
+                <b>{{ $contact->first_name }} {{ $contact->last_name }}</b> <small>({{ $contact->phone_number }})</small>
+                <form method="POST" action="{{ url("contacts/{$contact->id}") }}" style="display: inline">
+                    @csrf
+                    @method('DELETE')
+
+                    <button class="btn btn-outline-danger float-right mx-2">
+                        <i class="fa fa-trash"></i>
+                    </button>
+                </form>
+                <a href="#" class="btn btn-outline-warning float-right mx-2">
+                    <i class="fa fa-edit"></i>
+                </a>
+            </li>
+        @endforeach
+        </ul>
     </div>
 @stop
