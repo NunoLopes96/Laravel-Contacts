31f28d7670e8fb1c59febf913708c12dc74a0831
Nuno Lopes <nuno@redweb.dk>
2019-04-20 15:21:11 +0100

    Fix intlTelInput

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,10 +3,23 @@ intlTelInput = require('intl-tel-input');
 
 document.addEventListener('DOMContentLoaded', function(){
     var input = document.querySelector("#phone");
-    intlTelInput(input, {
+    var iti = intlTelInput(input, {
         utilsScript: require('intl-tel-input/build/js/utils'),
         initialCountry: "pt",
-        separateDialCode: 'true',
-        hiddenInput: input.getAttribute('name'),
+        autoHideDialCode: true,
+        nationalMode: false,
+        formatOnDisplay: true
     });
+
+    input.addEventListener("input", () => resetIntlTelInput());
+
+    function resetIntlTelInput() {
+        if (typeof intlTelInputUtils !== 'undefined') { // utils are lazy loaded, so must check
+            let currentText = iti.getNumber(intlTelInputUtils.numberFormat.E164);
+            if (typeof currentText === 'string') { // sometimes the currentText is an object :)
+                iti.setNumber(currentText); // will autoformat because of formatOnDisplay=true
+            }
+        }
+    }
+
 }, false);
