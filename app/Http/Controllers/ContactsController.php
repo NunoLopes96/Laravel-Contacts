d22dd1fd2444330c65f6074b97904f765250ac74
Nuno Lopes <nuno@redweb.dk>
2019-04-21 16:43:46 +0100

    Added authentication to access views

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -9,6 +9,16 @@ use App\Http\Requests\UpdateContactRequest;
 class ContactsController extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
