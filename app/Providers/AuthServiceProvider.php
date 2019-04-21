d7b29000e853641e36ebc4320c1253f4ec4500b5
Nuno Lopes <nuno@redweb.dk>
2019-04-21 18:57:25 +0100

    Register Policy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,8 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Gate;
+use App\Contact;
+use App\Policies\ContactPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -13,7 +14,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-         'App\Contact' => 'App\Policies\ContactPolicy',
+         Contact::class => ContactPolicy::class,
     ];
 
     /**
