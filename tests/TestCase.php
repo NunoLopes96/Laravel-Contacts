8d9c44f06e1df2454a525f79c17258d2e037a164
Nuno Lopes <nuno@redweb.dk>
2019-04-22 18:07:57 +0100

    Added SignIn function to TestCase to be used in tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,26 @@
 
 namespace Tests;
 
+use App\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * Creates a user if non provided and sign in with it.
+     *
+     * @param User|null $user - User to be loggedin.
+     *
+     * @return User
+     */
+    protected function signIn(User $user = null): User
+    {
+        $user = $user ?: factory('App\User')->create();
+
+        $this->actingAs($user);
+
+        return $user;
+    }
 }
